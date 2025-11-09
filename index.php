8237c9514ca4c5634d972b82f905eab437a71eea
Raketa10 <raketa1000000000@google.com>
2025-11-09 08:38:18 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -41,10 +41,23 @@
                     </div>
                 </div>
             </nav>
-            <section class="section-worlds">
+            <section class="section--games">
+                <h3>Játékok</h3>
+                <div class="status-message status-message--games"></div>
+                <div class="games-container">
+                    <!-- <div class="game">
+                        <div class="game-title">Utolsó játék folytatása</div>
+                        <div class="game-buttons">
+                            <img class="world-play svgbutton" src="./assets/images/btn-play.png"></img>
+                        </div>
+                    </div> -->
+                </div>
+            </section>
+            <section class="section--worlds">
                 <h3>Világok</h3>
+                <div class="status-message status-message--worlds"></div>
                 <div class="worlds-container">
-                    <div class="world">
+                    <!-- <div class="world">
                         <div class="world-title">Első világ</div>
                         <div>Kártyák: 9</div>
                         <div>Vezérkártyák: 3</div>
@@ -56,7 +69,7 @@
                     </div>
                     <div class="world world--add">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>
-                    </div>
+                    </div> -->
                 </div>
             </section>
         </div>
@@ -67,6 +80,8 @@
                 </button>
                 <h2>Világ létrehozása</h2>
                 <input type="text" name="world-name" maxlength="32" class="input world-name" placeholder="A világ neve">
+                <button class="button world-save-button">Mentés</button>
+                <div class="status-message status-message--world-save"></div>
             </div>
             <div class="world-grid">
                 <section class="section--worldcards">
@@ -156,6 +171,9 @@
                     </div>
                 </section>
             </div>
+        </div>
+        <div class="screen screen--game">
+
         </div>
     </div>
 
