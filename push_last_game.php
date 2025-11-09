e2753e56af6fdcced8286e801b1969cf979d023c
Raketa10 <raketa1000000000@google.com>
2025-11-09 17:03:19 +0100

    Wrote push_last_game.php

---
+++ push_last_game.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    require_once "connection.php";
+
+    // Get the world JSON:
+    $inputData = file_get_contents('php://input');
+    // -------------------
+
+    if(isset($_SESSION["user_id"])){
+        $statement = $connection->prepare("UPDATE users SET last_game_json = ? WHERE user_id = ?");
+        $statement->bind_param("si", $inputData, $_SESSION["user_id"]);
+        $statement->execute();
+    }
+?>
